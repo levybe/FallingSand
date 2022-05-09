01d97c85f74961e1968dcb66e8cca3bd52f4a3e6
levybe <levybe@ssfs.org>
2022-05-09 11:33:51 -0400

    Worked on SandLab up to creating the water particle. I need to modify my code so water will move either left, right, or down with each step call. Also, replace the integers of each tool with the final integers associated with them (ex. replace 2 with SAND).

--- SandLab.java
+++ SandLab.java
@@ -13,8 +13,9 @@ public class SandLab
   public static final int EMPTY = 0;
   public static final int METAL = 1;
   public static final int SAND = 2;
-  public static final int METALBGONE = 3;
-  public static final int SANDBGONE = 4;
+  public static final int WATER = 3;
+  public static final int METALBGONE = 4;
+  public static final int SANDBGONE = 5;
   
   //do not add any more fields
   private int[][] grid;
@@ -23,10 +24,11 @@ public class SandLab
   public SandLab(int numRows, int numCols)
   {
     String[] names;
-    names = new String[5];
+    names = new String[6];
     names[EMPTY] = "Empty";
     names[METAL] = "Metal";
     names[SAND] = "Sand";
+    names[WATER] = "Water";
     names[METALBGONE] = "Metal-B-Gone";
     names[SANDBGONE] = "Sand-B-Gone";
     display = new SandDisplay("Falling Sand", numRows, numCols, names);
@@ -46,19 +48,49 @@ public class SandLab
       for (int col = 0; col < grid[0].length; col++) {
         switch (grid[row][col]) {
           case 0: display.setColor(row, col, Color.black);
+                  break;
           case 1: display.setColor(row, col, Color.lightGray);
+                  break;
           case 2: display.setColor(row, col, Color.yellow);
-          case 3: display.setColor(row, col, metalBGoneColor);
-          case 4: display.setColor(row, col, sandBGoneColor);
+                  break;
+          case 3: display.setColor(row, col, Color.blue);
+                  break;
+          case 4: display.setColor(row, col, metalBGoneColor);
+                  break;
+          case 5: display.setColor(row, col, sandBGoneColor);
+                  break;
         }
       }
     }
   }
 
+  public boolean isValid (int row, int col) {
+    if (row >= 0 && row < grid.length && col >= 0 && col < grid[0].length) {
+      return true;
+    }
+    return false;
+  }
   //called repeatedly.
   //causes one random particle to maybe do something.
   public void step() {
-    
+    int row = (int) (Math.random() * grid.length);
+    int col = (int) (Math.random() * grid[0].length);
+
+    switch (grid[row][col]) {
+      case 2:
+        if (isValid(row + 1, col) && grid[row + 1][col] == 0) {
+        grid[row][col] = 0;
+        grid[row + 1][col] = 2;
+        }
+        break;
+      case 3:
+        int newCol = col + ((int) (Math.random() * 3) - 1);
+        if (isValid(row + 1, newCol) && grid[row + 1][newCol] == 0) {
+          grid[row][col] = 0;
+          grid[row + 1][newCol] = 3;
+        }
+      break;
+    }
   }
   
   //do not modify
